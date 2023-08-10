1d108950151ebc3ae0887163f99e0a66d9e7840f
Renato <renatomna@gmail.com>
2023-08-10 17:43:04 -0300

    fix: alterado nome da tabela fincionario_cargo

--- scripts/alters.sql
+++ scripts/alters.sql
@@ -1,8 +1,8 @@
 use `resilia`;
 
 -- INSERINDO CHAVE ESTRANGEIRA NA TABELA FUNCIONARIO
-alter table `funcionario` add `id_materia` int not null, add foreign key (`id_materia`) references `materia` (`id_materia`);
-alter table `funcionario` add `id_cadastro` int not null, add foreign key (`id_cadastro`) references `cadastro` (`id_cadastro`);
+alter table `funcionario_cargo` add `id_materia` int not null, add foreign key (`id_materia`) references `materia` (`id_materia`);
+alter table `funcionario_cargo` add `id_cadastro` int not null, add foreign key (`id_cadastro`) references `cadastro` (`id_cadastro`);
 
 -- INSERINDO CHAVE ESTRANGEIRA NA TABELA ESTUDANTE
 alter table `estudante` add `id_curso` int not null, add foreign key (`id_curso`) references `curso` (`id_curso`);
@@ -18,7 +18,7 @@ alter table `modulo` add `id_horario` int not null, add foreign key (`id_horario
 
 -- INSERINDO CHAVE ESTRANGEIRA NA TABELA CURSO
 alter table `curso` add `id_modulo` int not null, add foreign key (`id_modulo`) references `modulo` (`id_modulo`);
-alter table `curso` add `id_funcionario` int not null, add foreign key (`id_funcionario`) references `funcionario` (`id_funcionario`);
+alter table `curso` add `id_funcionario` int not null, add foreign key (`id_funcionario`) references `funcionario_cargo` (`id_funcionario`);
 
 -- INSERINDO CHAVE ESTRANGEIRA NA TABELA FINANCEIRO
 alter table `financeiro` add `id_estudante` int not null, add foreign key (`id_estudante`) references `estudante` (`id_estudante`);
@@ -37,7 +37,7 @@ alter table `nota` add `id_estudante` int not null, add foreign key (`id_estudan
 -- 4 horario OK
 -- 5 modulo OK
 -- 6 curso OK
--- 7 turma OK
+-- 7 turma OKs
 -- 8 estudante ?
 -- 9 nota ?
 -- 10 financeiro ok
