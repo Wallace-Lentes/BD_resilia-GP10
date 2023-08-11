67baee4061ad9cd2fc71b3b3c80c0aa1b8247680
Renato <renatomna@gmail.com>
2023-08-11 20:36:00 -0300

    feat: criação da view da pergunta 03.

--- scripts/perguntas.sql
+++ scripts/perguntas.sql
@@ -2,9 +2,40 @@
 select count(id_estudante) from estudante;
 
 -- pergunta 2: Selecionar quais facilitadores atuam em mais de uma turma.
-    SELECT 
-    id_funcionario, COUNT(id_funcionario) soma
+
+SELECT DISTINCT
+    cadastro.nome,
+    cadastro.Sobrenome,
+    contagem.id_funcionario,
+    funcionario_cargo.id_cadastro,
+    contagem.total_turmas
 FROM
-    turma
-GROUP BY id_funcionario
-HAVING soma > 1
+    (SELECT 
+        id_funcionario, COUNT(id_funcionario) total_turmas
+    FROM
+        turma
+    GROUP BY id_funcionario) contagem
+        JOIN
+    funcionario_cargo ON funcionario_cargo.id_cadastro = contagem.id_funcionario
+        LEFT JOIN
+    cadastro ON funcionario_cargo.id_cadastro = cadastro.id_cadastro
+WHERE
+    total_turmas > 1
+
+-- pergunta 3: Criar uma view que selecione a porcentagem de estudantes com status de evasão agrupados por turma.
+
+CREATE VIEW ViewPorcentagemEvasao AS
+    SELECT 
+        id_turma,
+        COUNT(CASE
+            WHEN status = 'inativo' THEN 1
+            ELSE NULL
+        END) AS total_inativos,
+        COUNT(*) AS total_alunos,
+        (COUNT(CASE
+            WHEN status = 'inativo' THEN 1
+            ELSE NULL
+        END) / COUNT(*)) * 100 AS porcentagem_inativos
+    FROM
+        estudante
+    GROUP BY id_turma;
